60948015034a971ca7cdc443d23b5559f5b9cf30
Yong Joon Lee <joon0351@gmail.com>
2018-10-28 16:33:53 +0900

    fix passwd

--- api/model/database.js
+++ api/model/database.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const database = new Sequelize(
     'account', // 테이블명
     'root', // 아이디
-    'wlsdml1103', // 비밀번호
+    'webtech', // 비밀번호
     {
         host: 'localhost',
         dialect: 'mysql',
