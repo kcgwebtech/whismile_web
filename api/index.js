03fcc3be6d2680d522ef11f0d7598c33dbd92ca6
Yong Joon Lee <joon0351@gmail.com>
2018-11-25 15:21:23 +0900

    append

--- api/index.js
+++ api/index.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const app = express();
-const port = 8888;
+const express = require('express'); // 모듈을 불러오는 구문
+const app = express(); // express 생성
+const port = 8888; // 서버를 열 때 사용되는 포트 정보
+
+const cookieParser = require('cookie-parser');
+const Token = require('./token');
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.get('/testAPI', (req, res) => {
+    res.json({
+        data: {
+            message: 'API 테스트 성공',
+        }
+    });
+});
+
 app.use('/auth', require('./auth'));
+app.use('/boards', require('./boards'));
 
-app.use(express.static('api'));
-
-app.get('/test', (req, res) => {
-    res.send('hi~');
-});
-
-app.listen(port, () => {
-    console.log('start server with 8888!');
-});
+app.listen(port); // port로 서버 열기
+console.log('start server with 8888!');
